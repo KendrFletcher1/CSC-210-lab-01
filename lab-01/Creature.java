5d49673829bb8225091636266e1182c5003e3300
KendrFletcher1 <KENDRA.FLETCHER001@STU.BMCC.CUNY.EDU>
2026-02-21 20:28:47 -0500

    Update Creature.java

--- lab-01/Creature.java
+++ lab-01/Creature.java
@@ -1,1 +1,30 @@
+public class Creature {
 
+    String name;
+    int size;
+
+    public Creature(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    public void move() {
+        System.out.println(name + " is moving.");
+    }
+
+    public void talk() {
+        System.out.println(name + " says hello!");
+    }
+
+    public static void main(String[] args) {
+        Creature creature1 = new Creature("Dragon", 10);
+
+        creature1.talk();
+        creature1.move();
+        creature1.eat();
+    }
+}
